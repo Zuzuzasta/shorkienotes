5f6d435ed67f71b78911e9c79157f7a483cfe436
Zuzuzasta <zuza.stachlewska@gmail.com>
2026-04-25 10:45:18 +0200

    Adding groups and fixing some bugs in Shopping tab

--- main/shopping.py
+++ main/shopping.py
@@ -31,6 +31,8 @@ class Shopping(qtw.QWidget):
     def add_produce_to_database_dialog(self):
         self.open_and_load_product_database()
         
+        self.add_produce_to_database_group_border = qtw.QGroupBox("Add produce to the database")
+        self.add_produce_to_database_group_border.setMaximumHeight(100)
         self.produce_dialog_layout = qtw.QHBoxLayout()
         self.produce_name_type_in = qtw.QLineEdit()
         self.produce_category_combo = qtw.QComboBox()
@@ -45,6 +47,7 @@ class Shopping(qtw.QWidget):
         self.produce_dialog_layout.addWidget(self.produce_name_type_in)
         self.produce_dialog_layout.addWidget(self.produce_category_combo)
         self.produce_dialog_layout.addWidget(produce_add_button)
+        self.add_produce_to_database_group_border.setLayout(self.produce_dialog_layout)
 
     def add_produce_to_database_button(self):
 
@@ -56,6 +59,7 @@ class Shopping(qtw.QWidget):
                 self.save_produce_to_file(produce_category, produce)
 
         self.apply_completer_to_line_edit()
+        self.choose_produce_combo_refresh()
 
     def save_produce_to_file(self, produce_category, produce):
 
@@ -71,32 +75,59 @@ class Shopping(qtw.QWidget):
 
     def add_produce_to_shopping_list_dialog(self):
         self.open_and_load_product_database()
-        
-        self.add_produce_layout = qtw.QHBoxLayout()
+        self.setup_produce_combo()
+        self.setup_produce_type_in()
 
-        self.add_produce_type_in = qtw.QLineEdit()
+        self.add_produce_to_list = qtw.QVBoxLayout()
+        self.add_produce_group_border = qtw.QGroupBox("Add produce to the shopping list")
+        self.add_produce_group_border.setMaximumHeight(500)
+        
+        or_label = qtw.QLabel("<< OR >>")
+        or_label.setMinimumHeight(50)
+        or_label.setAlignment(qtc.Qt.AlignmentFlag.AlignCenter)
+        or_label.setStyleSheet("QLabel{font-size: 20pt;}")
+
+        choose_label = qtw.QLabel("Choose produce from list")
+
+        type_in_label = qtw.QLabel("Type-in produce:")
+
+        self.add_produce_to_list.addWidget(choose_label)
+        self.add_produce_to_list.addLayout(self.add_produce_combo_select_layout)
+        self.add_produce_to_list.addWidget(or_label)
+        self.add_produce_to_list.addWidget(type_in_label)
+        self.add_produce_to_list.addLayout(self.add_produce_type_in_layout)
+        self.add_produce_group_border.setLayout(self.add_produce_to_list)
+
+    def setup_produce_combo(self):
+        self.add_produce_combo_select_layout = qtw.QHBoxLayout()
 
         self.choose_produce_category_combo = qtw.QComboBox()
         self.choose_produce_category_combo.addItems([produce_category for produce_category in self.product_database.keys()])
-
-        #? BTW QComboBox has a lineEdit option - maybe there is a need to rewrite the code a bit...
-        self.choose_produce_combo = qtw.QComboBox()
-        self.setup_for_choose_produce_combo()
-        
         self.choose_produce_category_combo.currentIndexChanged.connect(self.choose_produce_combo_refresh)
 
-        self.add_produce_to_list_button = qtw.QPushButton("Add to shopping list")
-        self.add_produce_to_list_button.pressed.connect(self.appending_produce_to_shopping_list_button)
+        self.choose_produce_combo = qtw.QComboBox()
+        self.setup_for_choose_produce_combo()
+
+        self.add_combo_produce_to_list_button = qtw.QPushButton("Add to shopping list")
+        self.add_combo_produce_to_list_button.pressed.connect(self.appending_combo_produce_to_shopping_list_button)
+
+        self.add_produce_combo_select_layout.addWidget(self.choose_produce_category_combo)
+        self.add_produce_combo_select_layout.addWidget(self.choose_produce_combo)
+        self.add_produce_combo_select_layout.addWidget(self.add_combo_produce_to_list_button)
+
+    def setup_produce_type_in(self):
+        self.add_produce_type_in_layout = qtw.QHBoxLayout()
+        self.add_produce_type_in = qtw.QLineEdit()
+
+        self.add_typed_produce_to_list_button = qtw.QPushButton("Add to shopping list")
+        self.add_typed_produce_to_list_button.pressed.connect(self.appending_typed_produce_to_shopping_list_button)
         
-        #? not sure if pressing enter here will also submit the popup selection or does it override that... (.activated / .popup)
-        self.add_produce_type_in.returnPressed.connect(self.add_produce_to_list_button.click)
+        self.add_produce_type_in.returnPressed.connect(self.add_typed_produce_to_list_button.click)
 
         self.apply_completer_to_line_edit()
 
-        self.add_produce_layout.addWidget(self.choose_produce_category_combo)
-        self.add_produce_layout.addWidget(self.choose_produce_combo)
-        self.add_produce_layout.addWidget(self.add_produce_type_in)
-        self.add_produce_layout.addWidget(self.add_produce_to_list_button)
+        self.add_produce_type_in_layout.addWidget(self.add_produce_type_in)
+        self.add_produce_type_in_layout.addWidget(self.add_typed_produce_to_list_button)
 
     def apply_completer_to_line_edit(self):
         produce_list = self.generate_lit_of_produce()
@@ -112,9 +143,9 @@ class Shopping(qtw.QWidget):
 
     def choose_produce_combo_refresh(self):
         self.choose_produce_combo.clear()
-        self.add_produce_layout.removeWidget(self.choose_produce_combo)
+        self.add_produce_combo_select_layout.removeWidget(self.choose_produce_combo)
         self.setup_for_choose_produce_combo()
-        self.add_produce_layout.insertWidget(1,self.choose_produce_combo)
+        self.add_produce_combo_select_layout.insertWidget(1,self.choose_produce_combo)
 
     def generate_lit_of_produce(self) -> list:
         self.open_and_load_product_database()
@@ -124,7 +155,7 @@ class Shopping(qtw.QWidget):
             produce_list = [*produce_list, *produce]
         return produce_list
 
-    def appending_produce_to_shopping_list_button(self):
+    def appending_typed_produce_to_shopping_list_button(self):
 
         self.open_and_load_product_database()
         self.open_and_load_shopping_list()
@@ -144,6 +175,21 @@ class Shopping(qtw.QWidget):
         
         self.refresh_shopping_list_tree_view()
 
+    def appending_combo_produce_to_shopping_list_button(self):
+
+        self.open_and_load_shopping_list()
+
+        if self.choose_produce_category_combo.currentText() not in self.shopping_list.keys():
+            self.shopping_produce_list = []
+        else:
+            self.shopping_produce_list = self.shopping_list[self.choose_produce_category_combo.currentText()]
+
+        self.shopping_produce_list.append(self.choose_produce_combo.currentText())
+        self.shopping_list[self.choose_produce_category_combo.currentText()] = self.shopping_produce_list
+        self.save_produce_to_shopping_list(self.shopping_list)
+            
+        self.refresh_shopping_list_tree_view()
+
     def open_and_load_shopping_list(self):
         with open(os.path.join(self.current_path, "main", "config" , "shopping_list.json"), "r") as shopping_list_file:
             self.shopping_list: dict = json.load(shopping_list_file)
@@ -157,6 +203,9 @@ class Shopping(qtw.QWidget):
     # TODO: html file generator for sending as link / attachment with check boxes
 
     def construct_shopping_list_tree_display(self):
+        self.shopping_list_label = qtw.QLabel("Shopping List:")
+        self.shopping_list_label.setStyleSheet("QLabel{font-size: 20pt;}")
+
         self.shopping_tree = qtw.QTreeWidget()
         self.shopping_tree.setColumnCount(2)
         self.shopping_tree.setHeaderLabels(["Category","Produce"])
@@ -186,10 +235,14 @@ class Shopping(qtw.QWidget):
 
     def shopping_tab_layout(self):
         self.shopping_inputs_layout = qtw.QVBoxLayout()
-        self.shopping_inputs_layout.addLayout(self.produce_dialog_layout)
-        self.shopping_inputs_layout.addLayout(self.add_produce_layout)
+        self.shopping_inputs_layout.addStretch()
+        self.shopping_inputs_layout.addWidget(self.add_produce_to_database_group_border)
+        self.shopping_inputs_layout.addStretch()
+        self.shopping_inputs_layout.addWidget(self.add_produce_group_border)
+        self.shopping_inputs_layout.addStretch()
 
         self.shopping_list_layout = qtw.QVBoxLayout()
+        self.shopping_list_layout.addWidget(self.shopping_list_label)
         self.shopping_list_layout.addWidget(self.shopping_tree)
 
         self.shopping_main_layout = qtw.QHBoxLayout()
